60cb91d37903fbb1966483dd6ef41f87ffa9311f
zonblade <zonblade@gmail.com>
2021-12-04 01:45:39 +0700

    module handler

--- module/.system/autoload.php
+++ module/.system/autoload.php
@@ -1,10 +1,20 @@
 <?php
+function loadModule(){
+    $package = file_get_contents(MODL_FOLDER . "/module.json");
+    $package = json_decode($package);
+    return $package->system;
+}
+$autoload = loadModule();
 
 foreach (glob(__DIR__."/vendor/*/*.php") as $filename)
 {
-    include $filename;
+    $include = false;
+    foreach($autoload as $package){if(strpos($filename, $package) !== false):$include == true;endif;}
+    if($include == true):include $filename;endif;
 }
 foreach (glob(__DIR__."/vendor/*/*/*.php") as $filename)
 {
-    include $filename;
+    $include = false;
+    foreach($autoload as $package){if(strpos($filename, $package) !== false):$include == true;endif;}
+    if($include == true):include $filename;endif;
 }
