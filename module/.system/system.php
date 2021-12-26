092a35a265dd0c3c1e483df377cd5ad4f2d12c16
zonblade <zonblade@gmail.com>
2021-12-26 15:34:07 +0700

    fixing bad code

--- module/.system/system.php
+++ module/.system/system.php
@@ -28,61 +28,47 @@ $GLOBALS['database_setup'] = false;
 $GLOBALS['mongodb'] = false;
 $mongo_database  = [];
 $global_database = [];
-if(!is_array($e->setup->database)){
-    if($e->setup->database != false){
-        $engine = $e->setup->database->engine;
-        if($engine == 'mongo'){
-            foreach($e->setup->database->settings as $k=>$v){
-                $mongo_database = $mongo_database + [$v->instance => $v];
-            }
-            $GLOBALS['mongodb'] = $mongo_database;
-        }elseif($engine == 'mysql'){
-            foreach($e->setup->database->settings as $k=>$v){
-                $global_database = $global_database + [$v->instance => $v];
-            }
-            $GLOBALS['database_setup']  = $global_database;
-        }
-    }
-}else{
-    foreach($e->setup->database as $k=>$v){
-        $engine = $v->engine;
-        if($engine == 'mongo'){
-            foreach($v->settings as $k=>$v){
-                $mongo_database = $mongo_database + [$v->instance => $v];
-            }
-        }elseif($engine == 'mysql'){
-            foreach($v->settings as $k=>$v){
-                $global_database = $global_database + [$v->instance => $v];
-            }
-        }
-    }
-    $GLOBALS['mongodb']         = $mongo_database;
-    $GLOBALS['database_setup']  = $global_database;
-}
-if($e->setup->database == false){
-    $GLOBALS['database_setup']  = false;
-}
+if(!is_array($e->setup->database))
+    {if($e->setup->database != false)
+        {$engine = $e->setup->database->engine;
+        if($engine == 'mongo')
+            {foreach($e->setup->database->settings as $k=>$v)
+                {$mongo_database    = $mongo_database + [$v->instance => $v];}
+                $GLOBALS['mongodb'] = $mongo_database;}
+        elseif($engine == 'mysql')
+            {foreach($e->setup->database->settings as $k=>$v){
+                $global_database            = $global_database + [$v->instance => $v];}
+                $GLOBALS['database_setup']  = $global_database;}}}
+else
+    {foreach($e->setup->database as $k=>$v)
+        {$engine = $v->engine;
+        if($engine == 'mongo')
+            {foreach($v->settings as $k=>$v)
+                {$mongo_database = $mongo_database + [$v->instance => $v];}}
+        elseif($engine == 'mysql')
+            {foreach($v->settings as $k=>$v)
+                {$global_database = $global_database + [$v->instance => $v];}}}
+    $GLOBALS['mongodb']        = $mongo_database;
+    $GLOBALS['database_setup'] = $global_database;}
 
-function globalurl(){
-    return $GLOBALS['global_url'] . $GLOBALS['uri_folder'];
-}
-function urlfolder(){
-    return $GLOBALS['uri_folder'];
-}
-function dbsetup(){
-    return $GLOBALS['database_setup'];
-}
-function globapp(){
-    return $GLOBALS['installed_apps'];
-}
-function dbmongo(){
-    return $GLOBALS['mongodb'];
-}
+if($e->setup->database == false)
+    {$GLOBALS['database_setup']  = false;}
+
+function globalurl()
+    {return $GLOBALS['global_url'] . $GLOBALS['uri_folder'];}
+function urlfolder()
+    {return $GLOBALS['uri_folder'];}
+function dbsetup()
+    {return $GLOBALS['database_setup'];}
+function globapp()
+    {return $GLOBALS['installed_apps'];}
+function dbmongo()
+    {return $GLOBALS['mongodb'];}
 #DO NOT CHANGE
 #DO NOT CHANGE
 function htaccessCHANGE($base_folder)
 {
-    $ht_updates = fopen(ROOT_FOLDER . '/.htaccess', "w") or die("Unable to open file!");
+    $ht_updates = fopen(ROOT_FOLDER . '/.htaccess', "w") or die("Permission problem please set to 755 and chwon www-data!");
     $ht_new     = "
     php_value upload_max_filesize 64M
     php_value post_max_size 64M
@@ -108,12 +94,14 @@ function htaccessCHANGE($base_folder)
     fwrite($ht_updates, $ht_text);
     fclose($ht_updates);
 }
+/*
+    removing bad fun,
+    please delete .htaccess in root folder (NOT INSIDE MODULE)
+    after re initializing the path or domain
+*/
 if (!file_exists(ROOT_FOLDER . '/.htaccess')) {
     htaccessCHANGE($base_folder);
-} elseif (file_exists(ROOT_FOLDER . '/.htaccess') && strtotime(filemtime(ROOT_FOLDER . '/.htaccess')) <= strtotime('-5 Days', strtotime(date('d-m-Y')))) {
-    htaccessCHANGE($base_folder);
 }
-
 
 #DO NOT CHANGE
 include __DIR__ . '/log.php';
