68b726cdadc189631d64ff9feb19528a6e97a9d1
zonblade <zonblade@gmail.com>
2021-12-26 14:41:20 +0700

    fix null

--- module/manage.php
+++ module/manage.php
@@ -3,14 +3,14 @@ define("ROOT_FOLDER",'../');
 define("MODL_FOLDER",__DIR__);
 define("APPS"       ,__DIR__.'/apps');
 include __DIR__ . '/.system/system.php';
-$val = getopt(null, ["name:"]);
-$opt = getopt(null, ["opt:"]);
+$val = getopt('X', ["name:"]);
+$opt = getopt('X', ["opt:"]);
 if ($opt['opt'] == 'migrate') {
     if ($val == null) {
         foreach (glob(__DIR__ . "/apps/*/models.php") as $models) {
             include $models;
         }
-    } elseif ($val != null) {
+    } elseif ($val != 'X') {
         if (file_exists(__DIR__ . "/apps/" . $val['name'])) {
             foreach (glob(__DIR__ . "/apps/" . $val['name'] . "/models.php") as $models) {
                 include $models;
@@ -24,7 +24,7 @@ if ($opt['opt'] == 'migrate') {
 if ($opt['opt'] == 'createapp') {
     if ($val == null) {
         echo "Please specify the name : phpr-create yourapp_name \n";
-    } elseif ($val != null) {
+    } elseif ($val != 'X') {
         if (!file_exists(__DIR__ . "/apps/" . $val['name'])) {
             exec('mkdir -p apps/' . $val['name'] . ';cp -a .system/vendor/phpr/quick_setup/basic/* apps/' . $val['name'] . ';', $output);
             echo "\nApp [" . "\033[33m" . $val['name'] . "\033[0m" . "]"."\033[32m"." Created! "."\033[0m"."\n\nCommand Available : \n        phpr-migrate " . $val['name'] . "\n        phpr-delete " . $val['name'] . "\n\n";
@@ -38,7 +38,7 @@ if ($opt['opt'] == 'createapp') {
 if ($opt['opt'] == 'deleteapp') {
     if ($val == null) {
         echo "Please specify the name : phpr-create yourapp_name \n";
-    } elseif ($val != null) {
+    } elseif ($val != 'X') {
         if (file_exists(__DIR__ . "/apps/" . $val['name'])) {
             exec('rm -r apps/' . $val['name'] . ';', $output);
             echo "\n" . 'App [' . "\033[33m" . $val['name'] . "\033[0m" . '] '. "\033[31m" .'Deleted' . "\033[0m" . "\n\n";
